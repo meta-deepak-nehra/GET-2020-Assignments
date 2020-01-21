ac831249bc98856568a121b1b6badfe14fc51a85
Deepak Nehra <deepak.nehra@metacube.com>
2020-01-21 15:12:34 +0530

    Improved the  try-catch

--- SCFO/Assignment_4/TestArrOperation.java
+++ SCFO/Assignment_4/TestArrOperation.java
@@ -5,15 +5,12 @@ public class TestArrOperation
 
 
 	//This method is used to test the code of count the clumps in an array.
-
-	@Test
+	@Test(expected =AssertionError.class)
 	public void testForClump()
 	{
-		try
-		{
 			ArrOperation obj1 = new ArrOperation();
 			int resultClump = obj1.countOfClumps(new int[]{1, 2 ,2, 3, 4, 4});
-			assertEquals( 2 , resultClump);
+			assertEquals( 1 , resultClump);
 
 			resultClump = obj1.countOfClumps(new int[]{1,1,2,1,1});
 			assertEquals( 2 , resultClump);
@@ -23,23 +20,12 @@ public class TestArrOperation
 
 			resultClump = obj1.countOfClumps(new int[]{0,0,0,0,0});
 			assertEquals(1 , resultClump);
-		}
-
-		catch(AssertionError ex) 
-		{
-			System.out.println("Your array is empty in test for clumps.");
-		}
-
-
 	}
 
 	//This method is used to test the code of Split Array Index which means position at which array can be split.
-
-	@Test
+	@Test(expected =AssertionError.class)
 	public void testForSplitArray()
 	{
-		try
-		{
 			ArrOperation obj2 = new ArrOperation();
 			int resultSplitIndex = obj2.splitArrayIndex(new int[]{1, 1 , 1, 2, 1});
 			assertEquals(3 , resultSplitIndex);
@@ -52,72 +38,41 @@ public class TestArrOperation
 
 			resultSplitIndex = obj2.splitArrayIndex(new int[0]);
 			assertEquals(1 , resultSplitIndex );
-
-		}
-		catch(AssertionError ex)
-		{
-			System.out.println("Array is empty.");
-		}
-
 	}
-
-
 
 	//  This method is used to test the code of FixXY problem.
-
-	@Test
+	@Test(expected =AssertionError.class)
 	public void testForFixXY() 
 	{
-
 		ArrOperation obj3 = new ArrOperation();
-		try
-		{
-			int []resultFixXY = obj3.fixXY(new int[]{5, 4, 9, 4, 9, 5} , 4 , 5);
-			int []expectedResult = new int []{9,4,5,4,5,9};
-			assertArrayEquals( expectedResult , resultFixXY);
+		int []resultFixXY = obj3.fixXY(new int[]{5, 4, 9, 4, 9, 5} , 4 , 5);
+		int []expectedResult = new int []{9,4,5,4,5,9};
+		assertArrayEquals( expectedResult , resultFixXY);
 
+		resultFixXY = obj3.fixXY(new int[]{1, 4, 1, 5} , 4 , 5);
+		expectedResult = new int []{1, 4, 5, 1};
+		assertArrayEquals( expectedResult , resultFixXY);
 
-			resultFixXY = obj3.fixXY(new int[]{1, 4, 1, 5} , 4 , 5);
-			expectedResult = new int []{1, 4, 5, 1};
-			assertArrayEquals( expectedResult , resultFixXY);
-
-
-			resultFixXY = obj3.fixXY(new int[]{1, 4, 1, 5, 5, 4, 1} , 4 , 5);
-			expectedResult = new int []{1, 4, 5, 1, 1, 4, 5};
-			assertArrayEquals( expectedResult , resultFixXY);
-
-		}
-		catch(AssertionError ex)
-		{
-			System.out.println("Enter the correct information.");
-		}
+		resultFixXY = obj3.fixXY(new int[]{1, 4, 1, 5, 5, 4, 1} , 4 , 5);
+		expectedResult = new int []{1, 4, 5, 1, 1, 4, 5};
+		assertArrayEquals( expectedResult , resultFixXY);
 	}
 
-
 	// This method is used to test the code of count of largest mirror in an array.
-
-	@Test
+	@Test(expected =AssertionError.class)
 	public void testlargestMirror()
 	{
-		try
-		{
-			ArrOperation obj4 = new ArrOperation();
-			int resultLargestMirror = obj4.largestMirror(new int[]{1, 2, 3, 8, 9, 3, 2, 1});
-			assertEquals(3 , resultLargestMirror);
+		ArrOperation obj4 = new ArrOperation();
+		int resultLargestMirror = obj4.largestMirror(new int[]{1, 2, 3, 8, 9, 3, 2, 1});
+		assertEquals(3 , resultLargestMirror);
 
-			resultLargestMirror = obj4.largestMirror(new int[]{1, 2, 1, 4});
-			assertEquals(3 , resultLargestMirror );
+		resultLargestMirror = obj4.largestMirror(new int[]{1, 2,3,2, 1});
+		assertEquals(5 , resultLargestMirror );
 
-			resultLargestMirror = obj4.largestMirror(new int[]{1, 4, 5, 3, 5, 4, 1});
-			assertEquals(7 , resultLargestMirror );
+		resultLargestMirror = obj4.largestMirror(new int[]{1, 4, 5, 3, 5, 4, 1});
+		assertEquals(7 , resultLargestMirror );
 
-			resultLargestMirror = obj4.largestMirror(new int[]{7, 1, 4, 9, 7, 4, 1});
-			assertEquals(2 , resultLargestMirror );
-		}
-
-		catch(AssertionError ex)
-		{
-			System.out.println("Enter the correct input");
-		}
+		resultLargestMirror = obj4.largestMirror(new int[]{7, 1, 4, 9, 7, 4, 1});
+		assertEquals(2 , resultLargestMirror );
 	}
 }
