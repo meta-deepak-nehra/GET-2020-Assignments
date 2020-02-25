9971fda0595baa7a6ad5aea3d73bfeb7d24387fd
Deepak Nehra <deepak.nehra@metacube.com>
2020-02-25 16:59:45 +0530

    Spring Boot

---
+++ MailSpring/src/main/java/com/metacube/mailinginfo/mailSender/SmtpMailSender.java
@@ -0,0 +1,40 @@
+package com.metacube.mailinginfo.mailSender;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+//with component annotation
+@Component
+public class SmtpMailSender implements MailSender {
+
+	@Override
+	public void message() {
+		System.out.println("Message from SmtpMail");
+	}
+
+}
+
+//without component annotation
+//for config file
+//public class SmtpMailSender implements MailSender {
+//
+//	@Override
+//	public void message() {
+//		System.out.println("Message from SmtpMail");
+//	}
+//
+//}
+
+//for primary
+
+//@Component
+//@Primary
+//public class SmtpMailSender implements MailSender {
+//
+//	@Override
+//	public void message() {
+//		System.out.println("Message from SmtpMail");
+//	}
+//
+//}
